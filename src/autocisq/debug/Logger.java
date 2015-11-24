16a8d6af4d1ee6a3c0f3036432395e0e5038745d
larsac07 <larsac07@gmail.com>
2015-11-24 12:53:45 +0100

    add bug() in Logger

--- src/autocisq/debug/Logger.java
+++ src/autocisq/debug/Logger.java
@@ -3,6 +3,9 @@ package autocisq.debug;
 import org.eclipse.core.resources.IResource;
 
 public class Logger {
+
+	private final static String NL = System.lineSeparator();
+
 	public static void log(String message) {
 		System.out.println(message);
 	}
@@ -11,10 +14,17 @@ public class Logger {
 		log(object.toString());
 	}
 
+	public static void bug(String message) {
+		System.err.println(message);
+	}
+
+	public static void bug(Object object) {
+		System.err.println(object);
+	}
+
 	public static void cisqIssue(IResource file, int errorLineNumber, int startIndex, int endIndex,
 			String problemArea) {
-		log("Found error in " + file.getLocation().toString() + " at line " + errorLineNumber + ":");
-		log("Start index: " + startIndex + " End index: " + endIndex);
-		log(problemArea);
+		bug("Found error in " + file.getLocation().toString() + " at line " + errorLineNumber + ":" + NL
+				+ "Start index: " + startIndex + " End index: " + endIndex + NL + problemArea);
 	}
 }
