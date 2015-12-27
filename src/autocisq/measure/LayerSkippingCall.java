0357bec28bb1da9f8c0943744a3df9d68f938fd8
larsac07 <larsac07@gmail.com>
2015-12-27 03:00:52 +0100

    Add LayerSkippingCall measure

---
+++ src/autocisq/measure/LayerSkippingCall.java
@@ -0,0 +1,97 @@
+package autocisq.measure;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+
+import autocisq.JavaParserHelper;
+import autocisq.NoAncestorFoundException;
+import autocisq.models.FileIssue;
+import autocisq.models.Issue;
+
+/**
+ * The LayerSkippingCall class represents the CISQ Maintainability Measure 2: #
+ * of layer-skipping calls.
+ *
+ * @author Lars A. V. Cabrera
+ *
+ */
+public class LayerSkippingCall implements Measure {
+
+	private List<CompilationUnit> compilationUnits;
+	private Map<String, Integer> layerMap;
+
+	/**
+	 * Constructs a new LayerSkipppingCall measure with a list of compilation
+	 * units in the project and a map of layers.
+	 *
+	 * If either values passed through the parameters are null, new are created.
+	 *
+	 * The required layerMap is a map of which compilation units are assigned to
+	 * which layer. The compilation unit is represented as package name + class
+	 * name, e.g. java.util.List, and the layer is represented as a simple
+	 * integer, e.g. "layer 1" = 1.
+	 *
+	 * @param compilationUnits
+	 *            - a list of all the CompilationUnit objects in the current
+	 *            project.
+	 * @param layerMap
+	 *            - a map of which compilation units are assigned to which
+	 *            layer.
+	 */
+	public LayerSkippingCall(List<CompilationUnit> compilationUnits, Map<String, Integer> layerMap) {
+		if (compilationUnits == null) {
+			compilationUnits = new LinkedList<>();
+		}
+		if (layerMap == null) {
+			layerMap = new HashMap<>();
+		}
+
+		this.compilationUnits = compilationUnits;
+		this.layerMap = layerMap;
+	}
+
+	@Override
+	public List<Issue> analyzeNode(Node node, String fileString) {
+		List<Issue> issues = new LinkedList<>();
+		if (node instanceof MethodCallExpr) {
+			MethodCallExpr methodCall = (MethodCallExpr) node;
+			if (!JavaParserHelper.methodCallFromSameType(methodCall)) {
+				CompilationUnit methodCompilationUnit = JavaParserHelper.findMethodCompilationUnit(methodCall,
+						this.compilationUnits);
+				if (methodCompilationUnit != null) {
+					CompilationUnit methodCallCompilationUnit = null;
+					try {
+						methodCallCompilationUnit = JavaParserHelper.findNodeCompilationUnit(methodCall);
+					} catch (NoAncestorFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+					String methodClass = methodCompilationUnit.getPackage().getPackageName() + "."
+							+ methodCompilationUnit.getTypes().get(0).getName();
+					String methodCallClass = methodCallCompilationUnit.getPackage().getPackageName() + "."
+							+ methodCallCompilationUnit.getTypes().get(0).getName();
+
+					Integer methodLayer = this.layerMap.get(methodClass);
+					Integer methodCallLayer = this.layerMap.get(methodCallClass);
+
+					if (methodLayer != null) {
+						if (Math.abs(methodLayer - methodCallLayer) > 1) {
+							int[] indexes = JavaParserHelper.columnsToIndexes(fileString, node.getBeginLine(),
+									node.getEndLine(), node.getBeginColumn(), node.getEndColumn());
+							issues.add(new FileIssue(methodCall.getBeginLine(), indexes[0], indexes[1],
+									"Layer-Skipping Call", methodCall.toString(), methodCall));
+						}
+					}
+				}
+			}
+		}
+		return issues;
+	}
+}
---
+++ src/autocisq/measure/LayerSkippingCallTest.java
@@ -0,0 +1,62 @@
+package autocisq.measure;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import autocisq.IssueFinder;
+import autocisq.models.FileIssue;
+import autocisq.models.Issue;
+
+public class LayerSkippingCallTest {
+
+	private List<File> layerTestFiles;
+	private LinkedHashMap<String, Integer> layerMap;
+
+	@Before
+	public void setUp() throws Exception {
+		this.layerTestFiles = new LinkedList<>();
+		this.layerTestFiles.add(new File("res/test/layers/GUI.java"));
+		this.layerTestFiles.add(new File("res/test/layers/GUIUtils.java"));
+		this.layerTestFiles.add(new File("res/test/layers/SettingsGUI.java"));
+		this.layerTestFiles.add(new File("res/test/layers/ThemeManager.java"));
+		this.layerTestFiles.add(new File("res/test/layers/TsvToHtml.java"));
+		this.layerTestFiles.add(new File("res/test/layers/Parser.java"));
+
+		this.layerMap = new LinkedHashMap<>();
+		this.layerMap.put("no.uib.lca092.rtms.gui.GUI", 1);
+		this.layerMap.put("no.uib.lca092.rtms.gui.GUIUtils", 2);
+		this.layerMap.put("no.uib.lca092.rtms.gui.SettingsGUI", 3);
+		this.layerMap.put("no.uib.lca092.rtms.gui.ThemeManager", 4);
+		this.layerMap.put("no.uib.lca092.rtms.TsvToHtml", 5);
+		this.layerMap.put("no.uib.lca092.rtms.io.Parser", 6);
+		this.layerMap.put("no.uib.lca092.rtms.gui.ThemeManager2", 7);
+		this.layerMap.put("no.uib.lca092.rtms.TsvToHtml2", 8);
+		this.layerMap.put("no.uib.lca092.rtms.io.Parser2", 9);
+	}
+
+	@Test
+	public void findLayerSkippingCalls() {
+		Map<File, List<Issue>> layerIssuesMap = IssueFinder.getInstance().findIssues(this.layerTestFiles,
+				this.layerMap);
+
+		boolean found = false;
+		search: for (List<Issue> fileIssues : layerIssuesMap.values()) {
+			for (Issue issue : fileIssues) {
+				if (issue.getType().equals("Layer-Skipping Call") && issue instanceof FileIssue) {
+					found = true;
+					break search;
+				}
+			}
+		}
+		assertTrue(found);
+	}
+
+}
