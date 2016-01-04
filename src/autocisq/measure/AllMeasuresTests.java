ac46a265dd49f40bbe64643e533b9628a8c27cef
larsac07 <larsac07@gmail.com>
2016-01-04 18:24:37 +0100

    Add Maintainability Measure test suite

---
+++ src/autocisq/measure/AllMeasuresTests.java
@@ -0,0 +1,11 @@
+package autocisq.measure;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ EmptyExceptionHandlingBlockTest.class, HorizontalLayersTest.class, LayerSkippingCallTest.class })
+public class AllMeasuresTests {
+
+}
