cfc55b69bf5c9ff2fdcabf224415705d8534601e
larsac07 <larsac07@gmail.com>
2016-01-11 13:20:20 +0100

    Add FunctionCommentedOutInstructionsTest and ContinueOrBreakOutsideSwitchTest to AllMeasuresTest

--- src/autocisq/measure/AllMeasuresTests.java
+++ src/autocisq/measure/AllMeasuresTests.java
@@ -4,19 +4,22 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import autocisq.measure.maintainability.FunctionParametersTest;
+import autocisq.measure.maintainability.ContinueOrBreakOutsideSwitchTest;
+import autocisq.measure.maintainability.FileLOCTest;
+import autocisq.measure.maintainability.FunctionCommentedOutInstructionsTest;
 import autocisq.measure.maintainability.FunctionFanOutTest;
+import autocisq.measure.maintainability.FunctionParametersTest;
 import autocisq.measure.maintainability.HorizontalLayersTest;
 import autocisq.measure.maintainability.LayerSkippingCallTest;
 import autocisq.measure.maintainability.MethodDirectlyUsingFieldFromOtherClassTest;
-import autocisq.measure.maintainability.FileLOCTest;
 import autocisq.measure.maintainability.VariableDeclaredPublicTest;
 import autocisq.measure.reliability.EmptyExceptionHandlingBlockTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ EmptyExceptionHandlingBlockTest.class, HorizontalLayersTest.class, LayerSkippingCallTest.class,
-		FileLOCTest.class, FunctionParametersTest.class, VariableDeclaredPublicTest.class,
-		FunctionFanOutTest.class, MethodDirectlyUsingFieldFromOtherClassTest.class })
+		FileLOCTest.class, FunctionParametersTest.class, VariableDeclaredPublicTest.class, FunctionFanOutTest.class,
+		MethodDirectlyUsingFieldFromOtherClassTest.class, FunctionCommentedOutInstructionsTest.class,
+		ContinueOrBreakOutsideSwitchTest.class })
 public class AllMeasuresTests {
 
 }
