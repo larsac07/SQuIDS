8fdd4018c8a7b80c19dd460a94e186f68154cf4b
larsac07 <larsac07@gmail.com>
2016-01-19 17:05:59 +0100

    Use log() instead of bug() in logIssue(). I.e. use standard syso color instead of syse color.

--- src/autocisq/debug/Logger.java
+++ src/autocisq/debug/Logger.java
@@ -6,32 +6,32 @@ import autocisq.models.FileIssue;
 import autocisq.models.ProjectIssue;
 
 public class Logger {
-
+	
 	private final static String NL = System.lineSeparator();
-
+	
 	public static void log(String message) {
 		System.out.println(message);
 	}
-
+	
 	public static void log(Object object) {
 		log(object.toString());
 	}
-
+	
 	public static void bug(String message) {
 		System.err.println(message);
 	}
-
+	
 	public static void bug(Object object) {
 		System.err.println(object);
 	}
-
+	
 	public static void logIssue(IResource file, FileIssue issue) {
-		bug("Found " + issue.getType() + " error in " + file.getLocation().toString() + " at line "
+		log("Found " + issue.getType() + " error in " + file.getLocation().toString() + " at line "
 				+ issue.getBeginLine() + ":" + NL + "Start index: " + issue.getStartIndex() + " End index: "
 				+ issue.getEndIndex() + NL + issue.getProblemArea());
 	}
-
+	
 	public static void logIssue(IResource file, ProjectIssue issue) {
-		bug("Found " + issue.getType() + " error in project");
+		log("Found " + issue.getType() + " error in project");
 	}
 }
